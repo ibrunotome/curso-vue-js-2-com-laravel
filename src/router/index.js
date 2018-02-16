27b5b4f8723996793d44238275906a3e3c2d7869
Bruno Tomé <ibrunotome@gmail.com>
2018-02-16 16:03:17 -0200

    Criando componente de visualização

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import HelloWorld from '../components/HelloWorld'
 import AccountsList from '../components/Accounts/List'
+import AccountsView from '../components/Accounts/View'
 
 const routes = [
     {
@@ -11,6 +12,11 @@ const routes = [
         path: '/contas',
         name: 'AccountsList',
         component: AccountsList
+    },
+    {
+        path: '/contas/:id',
+        name: 'AccountsList',
+        component: AccountsView
     }
 ];
 
