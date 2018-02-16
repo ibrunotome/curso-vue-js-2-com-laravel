2b8d992b24843929c7555531e2680fbe54963dea
Bruno Tomé <ibrunotome@gmail.com>
2018-02-16 15:58:24 -0200

    Criando componente de listagem

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import AccountsList from '../components/Accounts/List'
 
 const routes = [
-  {
-    path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld
-  }
-]
+    {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    },
+    {
+        path: '/contas',
+        name: 'AccountsList',
+        component: AccountsList
+    }
+];
 
 export default routes
