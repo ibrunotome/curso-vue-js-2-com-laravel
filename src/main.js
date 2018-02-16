c5bd8cbd1c74dd309415d48fedc15efa21c83aa7
Bruno Tomé <ibrunotome@gmail.com>
2018-02-16 15:35:22 -0200

    Configurando Vue Routes, Vuex e Vue Resource

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import {sync} from 'vuex-router-sync'
+
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
-import router from './router'
+import routes from './router'
+import VuexStore from './vuex/store'
 
-console.log(process.env.SERVER)
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(VueRouter)
 
-Vue.config.productionTip = false
+Vue.http.options.root = process.env.SERVER
+
+const store = new Vuex.Store(VuexStore)
+
+const router = new VueRouter({
+  routes
+})
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: {App},
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: {App}
 })
